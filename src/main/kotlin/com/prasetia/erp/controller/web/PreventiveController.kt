3cd21a072f5ac279197023d48910512fe9ad93bd
junifar <junifar@gmail.com>
2018-08-13 16:25:16 +0700

    + Detail

--- src/main/kotlin/com/prasetia/erp/controller/web/PreventiveController.kt
+++ src/main/kotlin/com/prasetia/erp/controller/web/PreventiveController.kt
@@ -2,13 +2,20 @@ package com.prasetia.erp.controller.web
 
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller("Preventive Web Controller")
 class PreventiveController{
 
     @RequestMapping("/preventive")
-    fun index(model:Model): String{
+    fun indexPreventive(model:Model): String{
         return "preventive/index"
     }
+
+    @RequestMapping("/preventive/detail/{id}")
+    fun detailPreventive(model: Model, @PathVariable("id") id: Int): String{
+        return "preventive/detail"
+    }
+
 }
