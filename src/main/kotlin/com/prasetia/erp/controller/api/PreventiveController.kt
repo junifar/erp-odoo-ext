bf5e67bfa31c042369e1c559dbb83a1d2bcbea61
junifar <junifar@gmail.com>
2018-08-16 11:25:57 +0700

    + Cleaning unused code / comment

--- src/main/kotlin/com/prasetia/erp/controller/api/PreventiveController.kt
+++ src/main/kotlin/com/prasetia/erp/controller/api/PreventiveController.kt
@@ -17,37 +17,4 @@ class PreventiveController{
 
     @RequestMapping("/api/preventive_customer")
     fun getAllData()= repository.getAllData()
-//    fun getAllData(): MutableList<PreventiveCustomerYear> {
-//        var preventiveCustomerYear = mutableListOf<PreventiveCustomerYear>()
-//        val data = repository.getAllData()
-//        data.forEach {
-//            items->
-//            var check = false
-//            preventiveCustomerYear.forEach {if (it.tahun == items.tahun) check=true}
-//            if (!check) preventiveCustomerYear.add(PreventiveCustomerYear(items.tahun, null))
-//
-//            preventiveCustomerYear.forEach {
-//                val listDetail = mutableListOf<PreventiveCustomerMonth>()
-//                data.forEach{
-//                    detail ->
-//                    if(it.tahun == detail.tahun){
-//                        var check = false
-//                        listDetail.forEach { detailCheck -> if (detail.bulan == detailCheck.month) check=true }
-//                        if (!check) listDetail.add(PreventiveCustomerMonth(detail.bulan, null))
-//                    }
-//                }
-//                it.detail = listDetail
-//            }
-//
-//            preventiveCustomerYear.forEach {
-//                it.detail?.forEach {
-//                    it_detail->
-//                    val listDetail = mutableListOf<PreventiveCustomerYearDetail>()
-//                    data.forEach{ detail -> if ((it.tahun == detail.tahun) and (it_detail.month == detail.bulan)) listDetail.add(PreventiveCustomerYearDetail(detail.customer_name, detail.customer_id, detail.nilai_po)) }
-//                    it_detail.detail = listDetail
-//                }
-//            }
-//        }
-//        return preventiveCustomerYear
-//    }
 }
