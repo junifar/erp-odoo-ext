69317addfe57311f4aa4ee663fbcca42eb9561fc
junifar <junifar@gmail.com>
2018-08-30 10:50:04 +0700

    Change Port

--- src/main/kotlin/com/prasetia/erp/constant/GlobalConstant.kt
+++ src/main/kotlin/com/prasetia/erp/constant/GlobalConstant.kt
@@ -2,6 +2,6 @@ package com.prasetia.erp.constant
 
 class GlobalConstant{
     companion object {
-        const val BASE_URL = "http://localhost:8080/"
+        const val BASE_URL = "http://localhost:8888/"
     }
 }
