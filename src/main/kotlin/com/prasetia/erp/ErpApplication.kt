7dcc9f53819ef9cb3361ff50a7999e3074bf57f1
junifar <junifar@gmail.com>
2018-11-30 15:48:44 +0700

    Build For war archive

--- src/main/kotlin/com/prasetia/erp/ErpApplication.kt
+++ src/main/kotlin/com/prasetia/erp/ErpApplication.kt
@@ -2,9 +2,10 @@ package com.prasetia.erp
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
-class ErpApplication
+class ErpApplication: SpringBootServletInitializer()
 
 fun main(args: Array<String>) {
     runApplication<ErpApplication>(*args)
