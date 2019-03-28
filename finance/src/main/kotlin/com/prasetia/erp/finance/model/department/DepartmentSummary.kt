f98659b17ffc18c79581ae2ed5dcc33184186ddc
junifar <junifar@gmail.com>
2019-03-28 12:15:48 +0700

    Change module structure
    Divide into 3 modules:
    - accounting
    - direksi
    - finance

---
+++ finance/src/main/kotlin/com/prasetia/erp/finance/model/department/DepartmentSummary.kt
@@ -0,0 +1,16 @@
+package com.prasetia.erp.finance.model.department
+
+import javax.persistence.Entity
+import javax.persistence.Id
+
+@Entity
+class DepartmentSummary(
+        @Id
+        val id:Long,
+        val periode:Long?,
+        val nilai_budget:Double?,
+        val realisasi_budget:Double?,
+        val persent_budget:Double?
+){
+    constructor(): this(0,0,0.0,0.0,0.0)
+}
